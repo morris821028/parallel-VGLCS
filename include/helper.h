37272bb4ae8d268e562a7740882fc0ad4f4b1367
morris1028 <morris821028@gmail.com>
2016-11-20 09:56:00 +0800

    constant folding / constant propagation

--- include/helper.h
+++ include/helper.h
@@ -5,7 +5,7 @@
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 static inline int log2int(int x) {
-	return __builtin_clz((int) 1) - __builtin_clz(x); 
+	return 31 - __builtin_clz(x); 
 }
 
 struct ISMQ {
