b77f8c89eb7aebfbdb317497b1db519fbd35bdc0
morris1028 <morris821028@gmail.com>
2017-02-27 19:45:16 +0800

    ISMQ: add powerful test case for disjoint set

--- doc/examples/judgegirl-20009/ISMQ-disjoint.cpp
+++ doc/examples/judgegirl-20009/ISMQ-disjoint.cpp
@@ -1,44 +1,31 @@
 #include "ISMQ.h"
-#include <algorithm>
-#include <string.h>
-using namespace std;
-
-#define MAXN 16777216
-static uint32_t value[MAXN];
-static int32_t parent[MAXN];
-static int32_t weight[MAXN];
-static uint32_t leader[MAXN][2];
-static int lIdx, x;
-static int findp(int x) {
-	return parent[x] == x ? x : (parent[x] = findp(parent[x]));
+ 
+#define MAXN (1<<24)
+int lIdx, x;
+uint32_t value[MAXN], parent[MAXN];
+int32_t weight[MAXN], leader[MAXN];
+static int32_t findp(int32_t x) {
+    return parent[x] == x ? x : (parent[x] = findp(parent[x]));
 }
-static inline int joint(int l, int r, uint32_t V) {
-	if (weight[r] >= weight[l]) {
-		weight[r] += weight[l];
-		parent[l] = r;
-		return r;
-	} else {
-		weight[l] += weight[r];
-		parent[r] = l;
-		value[l] = V;
-		return l;
-	}
-}
-
 void init_ISMQ(int N) {
-	x = -1, lIdx = -1;
+    x = -1, lIdx = -1;
 }
 void append_ISMQ(uint32_t V) {
-	++x;
-	value[x] = V, parent[x] = x, weight[x] = 1;
-	int u = x;
-	for (; lIdx >= 0 && leader[lIdx][1] <= V; lIdx--)
-		u = joint(leader[lIdx][0], u, V);
-	++lIdx;
-	leader[lIdx][0] = u;
-	leader[lIdx][1] = V;
+    ++x;
+    value[x] = V, parent[x] = x;
+    int u = x, weightR = 1;
+    for (int32_t *v = leader + lIdx; lIdx >= 0 && value[*v] <= V; v--, lIdx--) {
+        if (weightR <= weight[lIdx])
+            u = (parent[u] = *v);
+        else
+            parent[*v] = u;
+        weightR = weight[lIdx] + weightR;
+    }
+    ++lIdx;
+    value[u] = V;
+    leader[lIdx] = u;
+    weight[lIdx] = weightR; 
 }
 uint32_t query_ISMQ(uint32_t L) {
-	return value[findp(L)];
+    return value[findp(L)];
 }
-
