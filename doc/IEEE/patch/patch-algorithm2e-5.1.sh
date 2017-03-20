c98e0b84701acf8710178798e572baeb7e859c96
Shiang-Yun Yang <morris821028@gmail.com>
2017-03-20 15:06:42 +0800

    Comment by Pangfeng

--- doc/IEEE/patch/patch-algorithm2e-5.1.sh
+++ doc/IEEE/patch/patch-algorithm2e-5.1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+set -e
+sudo mv /usr/share/texlive/texmf-dist/tex/latex/algorithm2e/algorithm2e.sty /usr/share/texlive/texmf-dist/tex/latex/algorithm2e/algorithm2e.sty-bak
+set +e
+sudo cp ./algorithm2e.sty /usr/share/texlive/texmf-dist/tex/latex/algorithm2e/
+
