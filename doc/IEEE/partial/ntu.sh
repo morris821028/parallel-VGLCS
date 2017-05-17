0450036deeab33a8d4fa36db6f6e40b228881b19
unknown <morris821028@gmail.com>
2017-05-17 13:51:40 +0800

    NTU: transaction version to master thesis script

---
+++ doc/IEEE/partial/ntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#############
+#
+# ./ntu.sh 
+#
+#############
+
+set -x
+
+mkdir -p ./ntu
+
+PATTERN_RHS=("section" "subsection" "subsubsection" "subsubsubsection")
+PATTERN_LHS=("chapter"    "section"    "subsection"    "subsubsection")
+
+for TEXFILE in *.tex;
+do
+	FILE_PREFIX=${TEXFILE%%.tex}
+	echo "Process ${TEXFILE} to save as ./ntu/${FILE_PREFIX}-ntu.tex"
+	cp ${TEXFILE} ./ntu/${FILE_PREFIX}-ntu.tex
+	for pattern_idx in "${!PATTERN_RHS[@]}"; 
+	do
+		printf "%s %s\n" "${PATTERN_RHS[$pattern_idx]}" "${PATTERN_LHS[$pattern_idx]}"
+		sed -i "s/\\\\${PATTERN_RHS[$pattern_idx]}{\([^}]*\)}/\\\\${PATTERN_LHS[$pattern_idx]}{\1}/g" ./ntu/${FILE_PREFIX}-ntu.tex
+	done
+	sed -i 's/\\begin{abstract}/\\begin{abstractEN}/g' ./ntu/${FILE_PREFIX}-ntu.tex
+	sed -i 's/\\end{abstract}/\\end{abstractEN}/g' ./ntu/${FILE_PREFIX}-ntu.tex
+	sed -i '1i %% AUTO-GENERATED FILES %%' ./ntu/${FILE_PREFIX}-ntu.tex
+#	sed -e 's/\\section{\([^}]*\)}/\\chapter{\1}/g' ${TEXFILE}  >./ntu/${FILE_PREFIX}-ntu.tex
+done
+
